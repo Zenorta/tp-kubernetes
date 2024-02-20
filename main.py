f99f3dee0a125c03589300e0def22a0f3cf411f7
PATE Thibaud <patet@EM-SIT-01.sdis08.fr>
2024-02-20 11:49:44 +0100

    Ajout du microservice

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+# Charger les variables d'environnement
+message = os.getenv("MESSAGE", "Default Message of the Day")
+app_port = int(os.getenv("APP_PORT", 3000))
+
+# Endpoint pour obtenir le message du jour (MOTD)
+@app.route('/motd', methods=['GET'])
+def get_motd():
+    return jsonify({"message": message})
+
+if __name__ == '__main__':
+    print(f'starting app on port {app_port}')
+    app.run(debug=True, port=app_port)
